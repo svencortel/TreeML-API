c3fd71f1764d2ca6d962b81a5ab73f8b0fb5c6a0
soumyapandawastaken <40232317+soumyapandawastaken@users.noreply.github.com>
2018-10-31 16:41:35 +0000

    Added ROC curve

--- DecisionTree/CompareAlgorithms.py
+++ DecisionTree/CompareAlgorithms.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
+from sklearn import metrics
 
 def Get_F_Score(trueLabels, predictedLabel):
     return f1_score(trueLabels, predictedLabel, average='macro')
@@ -7,3 +8,12 @@ def Get_F_Score(trueLabels, predictedLabel):
 def Get_ConfusionMatrix(trueLabels, predictedLabel):
     return confusion_matrix(trueLabels, predictedLabel)
 
+def Generate_ROC_Curve(trueLabels, probabiltyEstimateOfLabel):
+    fpr, tpr, _ = metrics.roc_curve(trueLabels,  probabiltyEstimateOfLabel)
+    auc = metrics.roc_auc_score(y_train, y_pred_proba)
+    plt.plot(fpr,tpr,label="ROC curve area = "+str(auc))
+    plt.xlabel("False Positve Rate")
+    plt.ylabel("True Positve Rate")
+    plt.title("ROC Curve")
+    plt.legend(loc=4)
+    plt.show() 
